1c8f24daa31c6ddb989ab6b475135143d195fc83
Jason Staten <jstaten07@gmail.com>
2014-08-22 11:42:42 -0600

    add a test... for testing.

--- Gemfile
+++ Gemfile
@@ -4,3 +4,8 @@ ruby "2.1.2"
 
 gem "sinatra"
 gem "puma"
+
+group :development, :test do
+  gem "rake"
+  gem "minitest"
+end
---
+++ spec/first_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'My awesome test' do
+  it 'must pass' do
+    true.must_equal true
+  end
+end
